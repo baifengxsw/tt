3c844c618f01cd17c29d496942099d8f71495a78
baifengxsw <1072396463@qq.com>
2018-11-10 08:20:43 +0800

    序列化文件提交

---
+++ hadoop_intel/hdfs/src/main/java/cn/cn/itcast04/sequence/Testsequence.java
@@ -0,0 +1,29 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Testsequence {
+    //序列化写操作
+    public  static  void main(String[] args) throws IOException {
+        save();
+    }
+
+    public static  void save() throws IOException {
+        Configuration con =  new Configuration();
+
+        FileSystem fs = FileSystem.get(con);
+        Path p = new Path("/usr/seq/1.seq");
+        SequenceFile.Writer writer = SequenceFile.createWriter(fs,con,p, IntWritable.class, Text.class);
+        for (int i = 0 ;i<100;i++){
+            writer.append(new IntWritable(i),new Text("temp"+i));
+        }
+        writer.close();
+    }
+}
